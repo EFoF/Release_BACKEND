e9afbd7bd03d16a6e45beab50636fe8093dd3c55
jay <wlguswhd0809@naver.com>
2023-07-12 23:19:13 +0900

    [ADD] SecurityUtil

---
+++ src/main/java/com/service/releasenote/global/util/SecurityUtil.java
@@ -0,0 +1,30 @@
+package com.service.releasenote.global.util;
+
+import com.service.releasenote.global.error.exception.NotSignInException;
+import com.service.releasenote.global.error.exception.UnAuthorizedException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+@Slf4j
+public class SecurityUtil {
+    // 인증된 멤버의 pk 값을 확인하는 메서드
+    public static Long getCurrentUserId() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+//        log.info("authentication:{}", authentication);
+
+        if (authentication == null) { // authentication 에 인증 정보가 없는 경우
+            throw new UnAuthorizedException();
+        }
+
+        if(authentication.getPrincipal().equals("anonymousUser")) { // 로그인 하지 않은 경우
+            throw new NotSignInException();
+        }
+
+        String name = authentication.getName();
+        Long userId = Long.valueOf(name);
+
+        return userId;
+    }
+}
