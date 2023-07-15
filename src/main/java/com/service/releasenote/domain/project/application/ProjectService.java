f6ca86c5976e1243e3f614edacf5225ba424eca2
minkyung <minkyung7365@gmail.com>
2023-07-15 16:43:28 +0900

    [ADD] 프로젝트 이름 겹칠 경우 예외처리

--- src/main/java/com/service/releasenote/domain/project/application/ProjectService.java
+++ src/main/java/com/service/releasenote/domain/project/application/ProjectService.java
@@ -5,12 +5,14 @@ import com.service.releasenote.domain.company.model.Company;
 import com.service.releasenote.domain.project.dao.ProjectRepository;
 import com.service.releasenote.domain.project.dto.ProjectDto;
 import com.service.releasenote.domain.project.exception.exceptions.CompanyNotFoundException;
+import com.service.releasenote.domain.project.exception.exceptions.DuplicatedProjectTitleException;
 import com.service.releasenote.domain.project.model.Project;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Slf4j
@@ -25,11 +27,20 @@ public class ProjectService {
     public ProjectDto.CreateProjectResponseDto createProject
             (ProjectDto.CreateProjectRequestDto createProjectRequestDto, Long company_id) {
 
-        // 회사가 존재하지 않는 경우
+        // 회사가 존재하지 않는 경우 예외 처리
         Company company = companyRepository.findById(company_id)
                 .orElseThrow(CompanyNotFoundException::new);
 
         Project newProject = createProjectRequestDto.toEntity(company);
+
+        // 해당 회사 프로젝트들 중 프로젝트의 이름이 이미 존재하는 경우 예외 처리
+        List<String> TitleByCompanyId = projectRepository.findTitleByCompanyId(company_id);
+        for (String s : TitleByCompanyId) {
+            if(newProject.getTitle().equals(s)){
+                throw new DuplicatedProjectTitleException();
+            }
+        }
+
         Project saveProject = projectRepository.save(newProject);
 
         return new ProjectDto.CreateProjectResponseDto().toResponseDto(saveProject);
--- src/main/java/com/service/releasenote/domain/project/dto/ProjectDto.java
+++ src/main/java/com/service/releasenote/domain/project/dto/ProjectDto.java
@@ -52,4 +52,7 @@ public class ProjectDto {
                     .build();
         }
     }
+
+    public static class FindMyProjectResponseDto {
+    }
 }
