6de06b6a6eca83f0bd4bc2eb4b03571dab495d55
minkyung <minkyung7365@gmail.com>
2023-07-17 13:22:59 +0900

    [REFACTOR]Exception 수정, 프로젝트 삭제 기능 추가

--- src/main/java/com/service/releasenote/domain/project/exception/handler/ProjectExceptionHandler.java
+++ src/main/java/com/service/releasenote/domain/project/exception/handler/ProjectExceptionHandler.java
@@ -1,22 +1,28 @@
 package com.service.releasenote.domain.project.exception.handler;
 
 import com.service.releasenote.domain.project.exception.exceptions.DuplicatedProjectTitleException;
+import com.service.releasenote.domain.project.exception.exceptions.NotOwnerProjectException;
 import com.service.releasenote.global.error.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
-import static com.service.releasenote.domain.project.exception.response.ProjectErrorResponse.DUPLICATED_PROJECT_TITLE;
+import static com.service.releasenote.domain.project.exception.response.ProjectErrorResponse.NOT_OWNER_PROJECT;
 
 @Slf4j
 @RestControllerAdvice
 public class ProjectExceptionHandler {
 
     @ExceptionHandler(DuplicatedProjectTitleException.class)
-    public final ResponseEntity<ErrorResponse> handleDuplicatedParticipateException(DuplicatedProjectTitleException ex, WebRequest request) {
-//        log.info(request.getDescription(false));
-        return DUPLICATED_PROJECT_TITLE;
+    public final ResponseEntity<String> handleDuplicatedProjectTitleException(DuplicatedProjectTitleException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(NotOwnerProjectException.class)
+    public final ResponseEntity<String> handleNotOwnerProjectException(NotOwnerProjectException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 }
