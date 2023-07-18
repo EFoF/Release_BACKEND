e4683ea23d73271cc6ba8938da0b26a0769333bf
minkyung <minkyung7365@gmail.com>
2023-07-18 13:00:20 +0900

    [ADD] 프로젝트에 멤버 추가

--- src/main/java/com/service/releasenote/domain/member/dao/MemberCompanyRepository.java
+++ src/main/java/com/service/releasenote/domain/member/dao/MemberCompanyRepository.java
@@ -2,6 +2,18 @@ package com.service.releasenote.domain.member.dao;
 
 import com.service.releasenote.domain.member.model.MemberCompany;
 import org.springframework.data.jpa.repository.JpaRepository;
+<<<<<<< Updated upstream
 
 public interface MemberCompanyRepository extends JpaRepository<MemberCompany, Long> {
+=======
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface MemberCompanyRepository extends JpaRepository<MemberCompany, Long> {
+
+    @Query(value = "SELECT mc.member_id FROM member_company mc WHERE mc.company_id = :company_id", nativeQuery = true)
+    List<Long> findMemberListByCompanyId(@Param("company_id")Long company_id);
+>>>>>>> Stashed changes
 }
--- src/main/java/com/service/releasenote/domain/member/dto/MemberProjectDTO.java
+++ src/main/java/com/service/releasenote/domain/member/dto/MemberProjectDTO.java
@@ -1,13 +1,17 @@
 package com.service.releasenote.domain.member.dto;
 
+import com.service.releasenote.domain.company.model.Company;
 import com.service.releasenote.domain.member.model.Member;
 import com.service.releasenote.domain.member.model.MemberProject;
 import com.service.releasenote.domain.member.model.Role;
+import com.service.releasenote.domain.project.dto.ProjectDto;
 import com.service.releasenote.domain.project.model.Project;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public class MemberProjectDTO {
@@ -26,4 +30,41 @@ public class MemberProjectDTO {
                     .build();
         }
     }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class AddProjectMemberRequestDto {
+        // 프로젝트 멤버 추가 요청 dto
+        private String email;
+
+        public MemberProject toEntity(Project project, Member member) {
+            return MemberProject.builder()
+                    .role(Role.MEMBER)
+                    .project(project)
+                    .member(member)
+                    .build();
+        }
+    }
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class AddProjectMemberResponseDto {
+        // 프로젝트 멤버 추가 응답 dto
+        private Long member_id;
+        private Long project_id;
+        private Role role;
+
+        public MemberProjectDTO.AddProjectMemberResponseDto toResponseDto(MemberProject memberProject) {
+            return AddProjectMemberResponseDto.builder()
+                    .member_id(memberProject.getMember().getId())
+                    .project_id(memberProject.getProject().getId())
+                    .role(memberProject.getRole())
+                    .build();
+        }
+    }
+
+
 }
