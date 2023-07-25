f487dd91cbfe093c614b8c0c63b20086b5fe0bb1
jay <wlguswhd0809@naver.com>
2023-07-25 13:14:53 +0900

    [ADD] Exception Handler

--- src/main/java/com/service/releasenote/domain/member/error/handler/MemberExceptionHandler.java
+++ src/main/java/com/service/releasenote/domain/member/error/handler/MemberExceptionHandler.java
@@ -1,10 +1,6 @@
 package com.service.releasenote.domain.member.error.handler;
 
-import com.service.releasenote.domain.member.error.exception.DeleteMemberPermissionDeniedException;
-import com.service.releasenote.domain.member.error.exception.DuplicatedProjectMemberException;
-import com.service.releasenote.domain.member.error.exception.UserNotFoundException;
-import com.service.releasenote.domain.member.error.exception.UserPermissionDeniedException;
-import com.service.releasenote.domain.project.exception.exceptions.ProjectPermissionDeniedException;
+import com.service.releasenote.domain.member.error.exception.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -29,6 +25,36 @@ public class MemberExceptionHandler {
 
     @ExceptionHandler(DeleteMemberPermissionDeniedException.class)
     public final ResponseEntity<String> handleDeleteMemberPermissionDeniedException(DeleteMemberPermissionDeniedException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(DeletedMemberException.class)
+    protected final ResponseEntity<String> handleDeletedMemberException(DeletedMemberException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(MemberAlreadyExistsException.class)
+    protected final ResponseEntity<String> handleMemberAlreadyExistsException(MemberAlreadyExistsException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(InvalidCredentialsException.class)
+    protected final ResponseEntity<String> handleInvalidCredentialsException(InvalidCredentialsException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(InvalidPasswordException.class)
+    protected final ResponseEntity<String> handleInvalidPasswordException(InvalidPasswordException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(InvalidTokenException.class)
+    protected final ResponseEntity<String> handleInvalidTokenException(InvalidTokenException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(DuplicatedPasswordException.class)
+    protected final ResponseEntity<String> handleDuplicatedPasswordException(DuplicatedPasswordException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 }
