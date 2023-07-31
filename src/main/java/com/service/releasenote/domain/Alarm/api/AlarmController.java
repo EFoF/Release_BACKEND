87333ecc163af0352fb8f71776940d91ae65c02e
Owen-Choi <oldstyle482@gmail.com>
2023-07-31 14:43:24 +0900

    [ADD] Swagger 관련 로직 추가

--- src/main/java/com/service/releasenote/domain/Alarm/api/AlarmController.java
+++ src/main/java/com/service/releasenote/domain/Alarm/api/AlarmController.java
@@ -1,7 +1,7 @@
 package com.service.releasenote.domain.Alarm.api;
 
 import com.service.releasenote.domain.Alarm.application.AlarmService;
-import io.swagger.annotations.Api;
+import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -18,6 +18,13 @@ public class AlarmController {
 
     private final AlarmService alarmService;
     @GetMapping("{projectId}/alarms")
+    @ApiOperation("api for get alarm")
+    @ApiImplicitParam(name = "onlyNew", value = "읽지 않은 알람만 표시", required = true,
+            dataType = "Boolean", paramType = "query", defaultValue = "true")
+    @ApiResponses({
+            @ApiResponse(code=200, message="요청 성공"),
+            @ApiResponse(code=404, message="존재하지 않는 memberProject")
+    })
     public AlarmInfoDto alarmDetails(
             @PathVariable(name = "projectId") Long projectId,
             @RequestParam(value = "onlyNew", defaultValue = "true") Boolean onlyNew) {
@@ -27,6 +34,12 @@ public class AlarmController {
         return alarmService.getAlarmDetailByProjectId(projectId);
     }
 
+    @GetMapping("{projectId}/alarms")
+    @ApiOperation("api for read alarm")
+    @ApiResponses({
+            @ApiResponse(code=200, message="요청 성공"),
+            @ApiResponse(code=404, message="존재하지 않는 memberProject")
+    })
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PostMapping("{projectId}/alarms")
     public void alarmRead(@PathVariable(name = "projectId") Long projectId) {
