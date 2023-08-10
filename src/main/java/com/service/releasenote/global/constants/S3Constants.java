5b900f547647f2505dd7e3bca0354a2a1296d200
jyeon <jeongyeon865@gmail.com>
2023-08-10 20:13:48 +0900

    [ADD] kic 오브젝트 스토리지 변경

--- src/main/java/com/service/releasenote/global/constants/S3Constants.java
+++ src/main/java/com/service/releasenote/global/constants/S3Constants.java
@@ -1,7 +1,7 @@
-package com.service.releasenote.global.constants;
-
-public class S3Constants {
-    public static final String DIRECTORY ="company";
-
-    public static final String PREFIX = "https://s3.ap-northeast-2.amazonaws.com/doklib/company/";
-}
+//package com.service.releasenote.global.constants;
+//
+//public class S3Constants {
+//    public static final String DIRECTORY ="company";
+//
+//    public static final String S3PREFIX = "https://s3.ap-northeast-2.amazonaws.com/doklib/company/";
+//}
