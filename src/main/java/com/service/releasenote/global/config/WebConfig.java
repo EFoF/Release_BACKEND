55615a5d00959d6cf4187c9552a538998e1db2cd
jay <wlguswhd0809@naver.com>
2023-08-14 13:19:26 +0900

    [REFACTOR] WebConfig 에 ILB 주소 추가 - test

--- src/main/java/com/service/releasenote/global/config/WebConfig.java
+++ src/main/java/com/service/releasenote/global/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://10.0.16.169:80", "http://61.109.214.64:80") // ILB 추가
+                .allowedOrigins("http://localhost:3000", "http://10.0.16.169:80") // ILB 추가
                 .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE", "HEAD", "OPTIONS")
                 .allowCredentials(true) // 인증 정보(쿠키, 인증 헤더 등)를 전송할 수 있도록 허용
                 .maxAge(3600) // Preflight 요청의 캐싱 시간을 설정, 1시간
