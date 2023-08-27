fb4cadec8e01ba1c2175e0d96d1c2f8fa4b9e5f3
Owen-Choi <oldstyle482@gmail.com>
2023-08-27 10:00:30 +0900

    [ADD] 로그 임시방편 2

--- src/main/java/com/service/releasenote/domain/category/api/CategoryController.java
+++ src/main/java/com/service/releasenote/domain/category/api/CategoryController.java
@@ -1,15 +1,22 @@
 package com.service.releasenote.domain.category.api;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.service.releasenote.domain.category.application.CategoryService;
+import com.service.releasenote.domain.project.api.ProjectController;
+import com.service.releasenote.global.log.CommonLog;
 import com.service.releasenote.global.util.SecurityUtil;
 import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 import static com.service.releasenote.domain.category.dto.CategoryDto.*;
 
@@ -20,7 +27,8 @@ import static com.service.releasenote.domain.category.dto.CategoryDto.*;
 public class CategoryController {
 
     private final CategoryService categoryService;
-
+    private final ObjectMapper objectMapper;
+    private static final Logger logger = LoggerFactory.getLogger(ProjectController.class);
     /**
      * 카테고리 생성 api
      * @param projectId
@@ -38,8 +46,10 @@ public class CategoryController {
     })
     public Long categoryAdd(
             @PathVariable(name = "project_id") Long projectId,
-            @RequestBody CategorySaveRequest categorySaveRequest) {
+            @RequestBody CategorySaveRequest categorySaveRequest) throws JsonProcessingException {
         Long currentMemberId = SecurityUtil.getCurrentMemberId();
+        CommonLog commonLog = new CommonLog(objectMapper.writeValueAsString(categorySaveRequest), "Post", LocalDateTime.now());
+        logger.info(objectMapper.writeValueAsString(commonLog));
         return categoryService.saveCategory(categorySaveRequest, projectId, currentMemberId);
     }
 
@@ -51,7 +61,9 @@ public class CategoryController {
     @ApiOperation("api for get categories by project id")
     @GetMapping("/api/companies/projects/{project_id}/categories")
     @ApiResponses({ @ApiResponse(code = 200, message = "요청 성공"), @ApiResponse(code = 404, message = "존재하지 않는 프로젝트")})
-    public CategoryInfoDto categoryList(@PathVariable(name = "project_id") Long projectId) {
+    public CategoryInfoDto categoryList(@PathVariable(name = "project_id") Long projectId) throws JsonProcessingException {
+        CommonLog commonLog = new CommonLog(projectId + " 번 프로젝트 하위 카테고리 모두 조회", "Get", LocalDateTime.now());
+        logger.info(objectMapper.writeValueAsString(commonLog));
         return categoryService.findCategoryByProjectId(projectId);
     }
 
@@ -85,7 +97,15 @@ public class CategoryController {
     @GetMapping("/api/categories/{category_id}")
     public CategoryResponseDto categoryDetails (
             @PathVariable(name = "category_id") Long categoryId,
-            @RequestParam(value = "developer", required = true, defaultValue = "false") Boolean isDeveloper) {
+            @RequestParam(value = "developer", required = true, defaultValue = "false") Boolean isDeveloper) throws JsonProcessingException {
+        String msg;
+        if(isDeveloper) {
+            msg = "개발자 - " + categoryId + " 번 카테고리 조회";
+        } else {
+            msg = "일반인 - " + categoryId + " 번 카테고리 조회";
+        }
+        CommonLog commonLog = new CommonLog(msg, "Post", LocalDateTime.now());
+        logger.info(objectMapper.writeValueAsString(commonLog));
         return categoryService.findCategoryByCategoryId(categoryId, isDeveloper);
     }
 
@@ -105,8 +125,10 @@ public class CategoryController {
     @DeleteMapping("/api/companies/projects/{project_id}/categories/{category_id}")
     public String categoryRemove(
             @PathVariable(name = "project_id") Long projectId,
-            @PathVariable(name = "category_id") Long categoryId) {
+            @PathVariable(name = "category_id") Long categoryId) throws JsonProcessingException {
         Long currentMemberId = SecurityUtil.getCurrentMemberId();
+        CommonLog commonLog = new CommonLog(categoryId + " 번 카테고리 삭제", "Delete", LocalDateTime.now());
+        logger.info(objectMapper.writeValueAsString(commonLog));
         return categoryService.deleteCategory(categoryId, projectId, currentMemberId);
     }
 
@@ -128,9 +150,11 @@ public class CategoryController {
     public CategoryModifyResponseDto categoryModify(
             @PathVariable(name = "project_id")Long projectId,
             @PathVariable(name = "category_id")Long categoryId,
-            @RequestBody CategoryModifyRequestDto modifyRequestDto) {
+            @RequestBody CategoryModifyRequestDto modifyRequestDto) throws JsonProcessingException {
         Long currentMemberId = SecurityUtil.getCurrentMemberId();
         categoryService.modifyCategory(modifyRequestDto, categoryId, projectId, currentMemberId);
+        CommonLog commonLog = new CommonLog(objectMapper.writeValueAsString(modifyRequestDto), "Put", LocalDateTime.now());
+        logger.info(objectMapper.writeValueAsString(commonLog));
         return categoryService.findCategoryAndConvert(categoryId);
     }
 
--- src/main/java/com/service/releasenote/domain/company/api/CompanyController.java
+++ src/main/java/com/service/releasenote/domain/company/api/CompanyController.java
@@ -1,10 +1,16 @@
 package com.service.releasenote.domain.company.api;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.service.releasenote.domain.company.application.CompanyService;
+import com.service.releasenote.domain.release.api.ReleaseController;
+import com.service.releasenote.global.log.CommonLog;
 import com.service.releasenote.global.util.SecurityUtil;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -12,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 import static com.service.releasenote.domain.company.dto.CompanyDTO.*;
 
@@ -23,6 +30,8 @@ import static com.service.releasenote.domain.company.dto.CompanyDTO.*;
 public class CompanyController {
 
     private final CompanyService companyService;
+    private final ObjectMapper objectMapper;
+    private static final Logger logger = LoggerFactory.getLogger(CompanyController.class);
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/api/companies")
@@ -31,27 +40,30 @@ public class CompanyController {
             @RequestPart(value="name") String name) throws IOException {
         Long currentMemberId = SecurityUtil.getCurrentMemberId();
         Long companyId = companyService.createCompany(image, name, currentMemberId);
-
+        CommonLog commonLog = new CommonLog(name + " 회사가 생성됨", "Post", LocalDateTime.now());
+        logger.info(objectMapper.writeValueAsString(commonLog));
         // TODO: 반환 데이터 협의
         return companyId;
     }
 
     @GetMapping("/api/companies")
-    public Page<CompanyResponseDTO> searchCompany(@RequestParam(value = "search", required = false, defaultValue = "") String name, Pageable pageable) {
+    public Page<CompanyResponseDTO> searchCompany(@RequestParam(value = "search", required = false, defaultValue = "") String name, Pageable pageable) throws JsonProcessingException {
         // 회사 이름이 비어있는 경우는 고려하지 않음
         Page<CompanyResponseDTO> companyList = companyService.findCompaniesByName(name, pageable);
-
+        CommonLog commonLog = new CommonLog(name + " 회사가 검색됨", "Get", LocalDateTime.now());
+        logger.info(objectMapper.writeValueAsString(commonLog));
         // TODO: 데이터 없는 경우 반환 데이터 협의
         return companyList;
     }
 
     // TODO: API 수정
     @GetMapping(value = "/api/companies/member/companies")
-    public Page<CompanyResponseDTO> findCompanyByMemberId(Pageable pageable) {
+    public Page<CompanyResponseDTO> findCompanyByMemberId(Pageable pageable) throws JsonProcessingException {
         // TODO: MemberCompany 고려
         Long currentMemberId = SecurityUtil.getCurrentMemberId();
         Page<CompanyResponseDTO> companyList = companyService.findCompaniesByMemberId(pageable, currentMemberId);
-
+        CommonLog commonLog = new CommonLog( "개발자 - "+ " 회사가 검색됨", "Get", LocalDateTime.now());
+        logger.info(objectMapper.writeValueAsString(commonLog));
         // TODO: 반환 데이터
         return companyList;
     }
@@ -62,15 +74,19 @@ public class CompanyController {
                                                   @RequestPart(value="name", required = false) String name) throws IOException {
         Long currentMemberId = SecurityUtil.getCurrentMemberId();
         UpdateCompanyResponseDTO updateCompany = companyService.updateCompany(company_id, image, name, currentMemberId);
+        CommonLog commonLog = new CommonLog(name + " 회사가 수정됨", "Put", LocalDateTime.now());
+        logger.info(objectMapper.writeValueAsString(commonLog));
 
         // TODO: 반환 데이터 협의
         return updateCompany;
     }
 
     @DeleteMapping("/api/companies/{company_id}")
-    public Long deleteCompany(@PathVariable Long company_id) {
+    public Long deleteCompany(@PathVariable Long company_id) throws JsonProcessingException {
         Long currentMemberId = SecurityUtil.getCurrentMemberId();
         Long deleteCompanyId = companyService.deleteCompany(company_id, currentMemberId);
+        CommonLog commonLog = new CommonLog( company_id+ "번 회사가 삭제됨", "Get", LocalDateTime.now());
+        logger.info(objectMapper.writeValueAsString(commonLog));
 
         // TODO: 반환 데이터 협의
         return deleteCompanyId;
